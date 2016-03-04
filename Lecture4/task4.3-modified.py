99f86b3cc1921d2b478f339388da27bfb0e125ee
Scandie <grizly.vl@gmail.com>
2016-03-04 15:16:03 +0200

    made task4_3-modified less sloppy

--- Lecture4/task4.3-modified.py
+++ Lecture4/task4.3-modified.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 our_number = int(raw_input("vvedit' chyslo : \n", ))
 print our_number
 print str(bin(our_number))[2:]
@@ -6,19 +7,18 @@ our_str = our_str.replace('0', ')')
 our_str = our_str.replace('1', '(')
 print our_str
 while not(our_str == ''):
-    sym = our_str.find('(')
-    #print sym
-    if sym == 0:                                  # Shukaemo index pershoi vidkritoy dujki '('
-        our_str = our_str.replace('(', '')        # Yaksho index = 0, to vidalyaemo element i shukaemo pershu zakrity dujku.
-        #print our_str                            # Yaksho vona e, to vidalyaemo ii tej. Yaksho ii nema to NO
-        sym = our_str.find(')')                   # Yaksho index pershoi vidritoy dujki '(' != 0 to NO
-        #print sym                                # V inshomu vypadku, yaksho kinchilis' elementy to YES
-        if sym == -1:
+    if our_str.count('(') != our_str.count(')'):  # Первірка на однакову кількість дужок
+        print 'NO'
+        break
+    sym = our_str.find('(')                       # Шукаємо позицію першої відкритої дужки '('
+    if sym == 0:                                  # Якщо вона не на початку послідовності, то NO
+        our_str = our_str.replace('(', '')        # Якщо на початку, то видаляємо її і шукаємо першу закриту дужку ')'
+        sym = our_str.find(')')                   # Якщо вона є, то видаляємо її теж. якщо її немає, то NO
+        if sym == -1:                             # В іншому випадку, якщо дужок більше немає, то YES
             print 'NO'
             our_str = ''
         else:
             our_str = our_str.replace(')', '')
-            #print our_str
             if our_str == '':
                 print 'YES'
     else:
