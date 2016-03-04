34c10e90cae0eba3c8bd9febb3eb045b050b17a3
Scandie <grizly.vl@gmail.com>
2016-03-04 02:39:25 +0200

    it still can deal with first 3 tasks, but now it can choose a longer corridor in a fork

--- Final-exam/maze-runner.py
+++ Final-exam/maze-runner.py
@@ -147,13 +147,81 @@ def maze_controller(maze):
             maze.turn_right()
             return False
 
+    def fork():
+
+        """
+        Checking ability to go right and left at the same time
+        """
+
+        if check_left_side():
+            maze.turn_right()
+            maze.turn_right()
+            maze.go()
+            maze.turn_left()
+            if check_right_side():
+                maze.turn_right()
+                maze.turn_right()
+                maze.go()
+                maze.turn_right()
+                return True
+                print 'Hmm so this is a fork...'
+            else:
+                return False
+
+    def choose_longer_corridor():
+
+        """
+        choose longer corridors between left and right
+        """
+        print 'I am choosing my corridor'
+        distance_left = 1
+        maze.turn_right()
+        while maze.go():
+            distance_left += 1
+        print 'left corridor is', distance_left, 'fields long!'
+        d_l = distance_left
+        maze.turn_right()
+        maze.turn_right()
+        while d_l != 1:
+            maze.go()
+            d_l -= 1
+        maze.turn_right()
+
+        distance_right = 1
+        maze.turn_left()
+        while maze.go():
+            distance_right += 1
+        print 'right corridor is', distance_right, 'fields long!'
+        if distance_left > distance_right:
+            print 'i choose left corridor!'
+            maze.turn_right()
+        else:
+            print 'i choose right corridor!'
+            d_r = distance_right
+            maze.turn_right()
+            maze.turn_right()
+            while d_r != 1:
+                maze.go()
+                d_r -= 1
+            maze.turn_right()
+            maze.turn_left()
+
+
+
+
+
+
+
     while not maze.found():
+        print fork.__doc__
         while maze.go():
             if maze.found():
                 return maze.found()
-            if check_right_side():
-                print ''
+            if fork():
+                choose_longer_corridor()
             #if check_left_side():
+                #print ''
+            #if check_right_side():
                 #print ''
         while not (maze.go()):
             print 'i am stuck'
@@ -173,5 +241,5 @@ def maze_controller(maze):
 print maze_controller(maze1), 1
 print maze_controller(maze2), 2
 print maze_controller(maze3), 3
-print maze_controller(maze4), 4
+#print maze_controller(maze4), 4
 #print maze_controller(maze5), 5
